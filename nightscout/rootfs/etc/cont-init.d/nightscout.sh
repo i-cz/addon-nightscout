f2db1b77da3f926eef53da8f0cde2cf371c83cdb
i-cz <ihlavaty.cz@gmail.com>
2024-01-25 14:54:24 +0100

    add libre up server

--- nightscout/rootfs/etc/cont-init.d/nightscout.sh
+++ nightscout/rootfs/etc/cont-init.d/nightscout.sh
@@ -9,6 +9,7 @@ readonly mongodb_uri=$(bashio::config 'mongodb_uri')
 readonly connect_source=$(bashio::config 'connect_source')
 readonly connect_link_up_username=$(bashio::config 'connect_link_up_username')
 readonly connect_link_up_password=$(bashio::config 'connect_link_up_password')
+readonly connect_link_up_server=$(bashio::config 'connect_link_up_server')
 
 bashio::log.info "Setting up API KEY: ${api_key}"
 export API_SECRET="${api_key}"
@@ -17,3 +18,4 @@ export MONGODB_URI="${mongodb_uri}"
 export CONNECT_SOURCE="${connect_source}"
 export CONNECT_LINK_UP_USERNAME="${connect_link_up_username}"
 export CONNECT_LINK_UP_PASSWORD="${connect_link_up_password}"
+export CONNECT_LINK_UP_SERVER="${connect_link_up_server}"
